f23c43470a215cb870643ba1e9fb5213206b9397
Daniel Pimenov <daniil.pimenov@gmail.com>
2023-05-03 01:47:47 +0400

    Add basic functionality (adding and loading drones) and tests

--- src/main/java/com/musala/drones/service/DronesService.java
+++ src/main/java/com/musala/drones/service/DronesService.java
@@ -2,7 +2,6 @@ package com.musala.drones.service;
 
 import com.musala.drones.dto.DroneDto;
 import com.musala.drones.dto.LoadedMedicationsRowDTO;
-import com.musala.drones.model.Drone;
 
 import java.util.List;
 
